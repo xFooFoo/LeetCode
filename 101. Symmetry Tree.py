ac202987344acbdea3e32502c9df551e1aa89c35
xFooFoo <yarivfu@gmail.com>
2026-03-22 10:03:18 +1300

    new easy tree solutions

---
+++ 101. Symmetry Tree.py
@@ -0,0 +1,44 @@
+'''
+101. Symmetric Tree
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def isMirror(left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:
+            if not left or not right:
+                return left == right
+            return left.val == right.val and isMirror(left.left, right.right) and isMirror(left.right, right.left)
+
+        # Empty tree is considered symmetrical
+        if not root:
+            return True
+        return isMirror(root.left, root.right)
+
